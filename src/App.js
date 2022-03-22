9bc2f8c3112c654e15ac80746db55e46ad6d6482
yuetong3yu <yuetong3yu@gmail.com>
2022-03-22 17:45:39 +0800

    user agent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [userAgent, setUserAgent] = useState('')
+
+  useEffect(() => {
+    setUserAgent(navigator.userAgent)
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Your Device: </h1>
+      <h3>{userAgent}</h3>
     </div>
   )
 }
