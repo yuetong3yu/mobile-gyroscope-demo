c39bbfa706da031841687152f16741b45b2af9bd
yuetong3yu <yuetong3yu@gmail.com>
2022-03-23 13:35:29 +0800

    ✨ update

--- src/App.js
+++ src/App.js
@@ -45,16 +45,43 @@ function App() {
     window.removeEventListener('deviceorientation', handleDeviceOrientation)
   }
 
+  const handleAddAcceleroClick = () => {
+    if (
+      AccelerometerEvent &&
+      typeof AccelerometerEvent.requestPermission === 'function'
+    ) {
+      AccelerometerEvent.requestPermission()
+        .then((response) => {
+          if (response === 'granted') {
+            window.addEventListener('devicemotion', handleDeviceOrientation)
+          }
+        })
+        .catch(console.error)
+    }
+  }
+
+  const handleRemoveAcceleroClick = () => {
+    window.removeEventListener('devicemotion', handleDeviceOrientation)
+  }
+
   return (
     <div className="App">
-      <button onClick={handleAddClick}>add listener</button>
-      <button onClick={handleRemoveClick}>remove listener</button>
+      <button onClick={handleAddClick}>add gyroscope listener</button>
+      <button onClick={handleRemoveClick}>remove gyroscope listener</button>
+      <button onClick={handleAddAcceleroClick}>
+        add accelerometer listener
+      </button>
+      <button onClick={handleRemoveAcceleroClick}>
+        remove accelerometer listener
+      </button>
       <h1>Your Device: </h1>
       <h3>{userAgent}</h3>
       <br />
+      <h2>陀螺仪</h2>
       <h3>水平旋转: {deviceOrientation.alpha}</h3>
       <h3>竖直翻转: {deviceOrientation.beta}</h3>
       <h3>水平翻转: {deviceOrientation.gamma}</h3>
+      <br />
     </div>
   )
 }
